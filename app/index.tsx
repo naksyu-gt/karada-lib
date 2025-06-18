44458209624bdb8367ab908be35bb51129fba385
naksyu-gt <nakasyu_gt@icloud.com>
2025-06-18 11:46:35 +0900

    fix/IOSで起動しなかったので修正

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 export default function Page() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-lg font-bold">Hello World!</Text>
-    </View>
+    <StyledView className="flex-1 justify-center items-center bg-white">
+      <StyledText className="text-lg font-bold text-red-600">Hello World!</StyledText>
+    </StyledView>
   );
 } 
